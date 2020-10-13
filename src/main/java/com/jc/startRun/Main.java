179862c487ed37a3f1345f69a847e4bacc3fd564
JamesChang <jameschang@wezoomteko>
2020-10-13 14:43:37 +0800

    feat:新增varcher * 4 功能

--- src/main/java/com/jc/startRun/Main.java
+++ src/main/java/com/jc/startRun/Main.java
@@ -1,26 +1,70 @@
 package com.jc.startRun;
 
 import com.jc.db.LoadDBContent;
+import com.jc.turn.JcFile;
 import org.apache.logging.log4j.LogManager;
+
+import java.io.IOException;
+
+import static java.lang.System.out;
 
 /**
  * 流程控制
  */
-public class Main {
+public class Main  {
     private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
+    //參數設定:設定轉碼目標
+    private final static String trunEncode = "UTF-8";
 
-        var load = new LoadDBContent();
+    //參數設定:起始目錄
+    private final static String startPath = "E:/Desktop/db2/";
+    //參數設定:篩選檔案
+    private final static String pFilter = ".sql";
+    //參數設定:取代路徑
+    private final static String replacePath = "E:/Desktop/finish/";
+    //參數設定:目的路徑
+    private final static String purposePath = "E:/Desktop/finish/";
 
-        LOGGER.info("初始化設定...");
-        load.setConnents();
-        LOGGER.info("開始讀取資料庫，並寫入檔案");
-        LOGGER.info("正在取得 Functions...");
-        load.getFunctions();
-        LOGGER.info("正在取得 Procedures...");
-        load.getProcudres();
-        LOGGER.info("資料讀取結束");
+    public static void main(String[] args) throws Exception {
+
+//        var load = new LoadDBContent();
+
+//        LOGGER.info("初始化設定...");
+//        load.setConnents();
+//        LOGGER.info("開始讀取資料庫，並寫入檔案");
+//        LOGGER.info("正在取得 Functions...");
+//        load.getFunctions();
+//        LOGGER.info("正在取得 Procedures...");
+//        load.getProcudres();
+//        LOGGER.info("資料讀取結束");
+
+        //取得要轉檔的檔案路徑
+        JcFile.getPathDir(startPath,pFilter);
+
+        LOGGER.info("startPath = "+startPath);
+        LOGGER.info("pFilter = "+pFilter);
+
+        //執行
+        for( String fileSourceDir : JcFile.gFileList ){
+
+            LOGGER.info("replacePath = "+replacePath);
+            LOGGER.info("purposePath = "+purposePath);
+
+
+            String goalFileDir = fileSourceDir.replace(startPath,purposePath);
+
+            String code = JcFile.checkEncoding(fileSourceDir);
+
+            if(code != null){
+                LOGGER.info("goalFileDir = "+goalFileDir);
+                JcFile.creatFile(goalFileDir);
+
+                //寫入檔案內容
+                JcFile.convertFileCode(fileSourceDir,goalFileDir,code,trunEncode);
+            }
+
+        }
 
 
     }
---
+++ src/main/java/com/jc/turn/JcFile.java
@@ -0,0 +1,201 @@
+package com.jc.turn;
+
+import org.mozilla.universalchardet.UniversalDetector;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ *  File Description: 檔案處理
+ *  @author   JamesChang
+ *  @since   2020-03-20111:41 OpenJDK 14
+ */
+public class JcFile {
+
+
+    /** 檔案集合 */
+    public static List<String> gFileList = new ArrayList<String>();
+
+    /**
+     * 建立檔案
+     * @param path
+     * @throws IOException
+     */
+    public static void creatFile(String path) throws IOException {
+        String checkPath = path.substring(0,path.lastIndexOf("/"));
+        java.io.File newPath = new java.io.File( checkPath );
+        //判斷目錄是否存在，不存在則建立
+        if(!newPath.exists()){
+            newPath.mkdirs();
+        }
+        //判斷檔案是否存在，不存在則建立
+        java.io.File newFile = new java.io.File( path );
+        if(!newFile.exists()){
+            newFile.createNewFile();
+        }
+    }
+
+    /**
+     * 取得要處理的檔案
+     * @param startPath
+     * @param fileFilter
+     */
+    public static void getPathDir(String startPath,String fileFilter){
+        File topPathFile = new File( startPath );
+        String[] filePathArray = topPathFile.list();
+        if(filePathArray != null){
+            for(int i=0 ; i< filePathArray.length ; i++){
+                String filePath = startPath+"/"+ filePathArray[i];
+                File file = new File( filePath);
+                //判斷是否為檔案
+                if(file.isFile()){
+                    if( fileFilter.equals("*") || file.getPath().indexOf(fileFilter) >= 0 )
+                        gFileList.add( startPath + "/" + filePathArray[i] );
+                }
+                else {
+                    getPathDir(filePath,fileFilter);
+                }
+            }
+        }
+        else {
+            gFileList.add( startPath );
+        }
+
+    }
+
+
+    /**
+     * 確認檔案編碼
+     * @param filePath 檔案路徑
+     * @return [String]charset:編碼格式
+     * @throws IOException
+     */
+    public static String checkEncoding(String filePath) throws IOException {
+
+        /* 讀取檔案 */
+        FileInputStream fis = new FileInputStream(filePath);
+        /* 建立分析器 */
+        UniversalDetector detector = new UniversalDetector(null);
+
+        int nread;
+        byte[] buf = new byte[4];
+        while ((nread = fis.read(buf)) > 0 && !detector.isDone()) {
+            /* 分析資料 */
+            detector.handleData(buf, 0, nread);
+        }
+        fis.close();
+        detector.dataEnd();
+        /* 取得編碼格式 */
+        String charset = detector.getDetectedCharset();
+
+        /*預設ANSI*/
+        if(charset == null){
+            charset = "Cp1252";
+        }
+
+        return charset;
+    }
+
+
+    /**
+     * 尋找資料，模糊查詢
+     * @param filePath 目標檔案路徑
+     * @param fileCode 檔案編碼
+     * @param keyWord 查詢關鍵字
+     * @return 有找到
+     * @throws IOException
+     */
+    public static boolean searchFileContent(String filePath,String fileCode, String keyWord) throws Exception {
+
+        if(filePath == null){throw new Exception("The function searchFileContent param [filePath] can't null ");}
+        if(fileCode == null){throw new Exception("The function searchFileContent param [fileCode] can't null ");}
+        if(keyWord == null){throw new Exception("The function searchFileContent param [keyWord] can't null ");}
+
+        keyWord = keyWord.toUpperCase();
+        /*去除空格*/
+        keyWord = keyWord.replaceAll("\\s*", "");
+
+        boolean isFind = false;
+
+        BufferedReader br = new BufferedReader( new InputStreamReader(new FileInputStream(filePath), fileCode));
+
+        List<String> lines ;
+
+        lines = br.lines().collect(Collectors.toList());
+
+        for (String line : lines) {
+            line = line.toUpperCase();
+            /*去除空格*/
+            line = line.replaceAll("\\s*", "");
+
+            if(line.indexOf(keyWord.toUpperCase()) != -1){
+                isFind = true;
+                break;
+            }
+        }
+        return isFind;
+    }
+
+    /**
+     * 轉碼程式
+     * @param fileSourceDir 檔案來源
+     * @param goalFileDir 存放檔案目標路徑
+     * @param code 原檔編碼
+     * @param trunEncode 目標編碼
+     * @throws IOException
+     */
+    public static void convertFileCode(String fileSourceDir, String goalFileDir,String code,String trunEncode) throws IOException {
+
+        String keyWord = "VARCHAR(";
+        keyWord = keyWord.toUpperCase();
+        /*去除空格*/
+        keyWord = keyWord.replaceAll("\\s*", "");
+
+        BufferedReader br = new BufferedReader( new InputStreamReader(new FileInputStream(fileSourceDir), code));
+
+        PrintWriter pw = new PrintWriter( new OutputStreamWriter(new FileOutputStream(goalFileDir), trunEncode));
+
+        String line = br.readLine();
+
+        String regEx="[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        StringBuilder rLine = new StringBuilder();
+
+        while (line != null) {
+
+            String[] conpareLineL = line.split("\t");
+
+            rLine = new StringBuilder();
+
+            for(String conpareLineI:conpareLineL){
+                if(conpareLineI.indexOf(keyWord.toUpperCase()) != -1){
+
+                    Matcher m = p.matcher(conpareLineI);
+
+                    System.out.println("conpareLineI = "+conpareLineI);
+                    String oInt = m.replaceAll("").trim();
+                    String nInt = Integer.toString((Integer.parseInt(m.replaceAll("").trim())*4));
+                    System.out.println("O = "+conpareLineI);
+                    System.out.println("N = "+conpareLineI.replace(oInt,nInt));
+                    rLine.append(conpareLineI.replace(oInt,nInt));
+                }
+                else{
+                    rLine.append(conpareLineI);
+                }
+                rLine.append("\t");
+            }
+            rLine.append("\r\n");
+
+            pw.write(rLine.toString());
+            line = br.readLine();
+        }
+
+        pw.flush();
+        pw.close();
+
+    }
+}
